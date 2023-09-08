ae7a9f4dfad41103fa07056ed9a69b815d8a94d6
zodplugin <zodplugin@gmail.com>
2023-09-08 14:51:51 +0700

    refactor: user validation unique no_telp

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'no_telp' => ['required', 'string', 'max:15'],
+            'no_telp' => ['required', 'string', 'max:15','unique:users'],
             'provinsi' => ['required', 'string'],
             'kota' => ['required', 'string'],
             'kecamatan' => ['required', 'string'],
