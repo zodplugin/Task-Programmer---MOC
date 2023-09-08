7c1f4be3659a94e5cbe512e587b67536265e96f6
zodplugin <zodplugin@gmail.com>
2023-09-08 15:51:30 +0700

    refactor: env + readme

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -95,7 +95,7 @@ class HomeController extends Controller
 
 
         $otp = Str::upper(Str::random(5));
-        $res = Http::get('http://47.251.18.83/send/'. env('TOKEN_API') .'/'.$user->no_telp,[
+        $res = Http::get('http://47.251.18.83/send/'. env('TOKEN_API','TIDAKADA') .'/'.$user->no_telp,[
             'text' => $otp
         ]);
 
